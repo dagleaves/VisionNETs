faaf92a0cb9ab3bbc00cdfd87baeb54c9d6180f0
Daniel Gleaves <68761152+dagleaves@users.noreply.github.com>
2023-03-28 23:44:04 -0400

    Add AVG pooling to AlexNet

--- models/AlexNet.py
+++ models/AlexNet.py
@@ -26,6 +26,7 @@ class AlexNet(nn.Module):
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
 
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5),
             nn.Linear(in_features=256 * 6 * 6, out_features=4096),
@@ -38,6 +39,7 @@ class AlexNet(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
+        x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
