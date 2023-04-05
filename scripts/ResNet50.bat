985eab00f241e4973e34d5ce7ab8881f9aca2417
Daniel Gleaves <68761152+dagleaves@users.noreply.github.com>
2023-04-04 21:27:02 -0400

    Remove batch script

--- scripts/ResNet50.bat
+++
@@ -1,3 +0,0 @@
-python train.py --model ResNet50 --dataset FashionMNIST --epochs 15
-python train.py --model ResNet50 --dataset CIFAR10 --epochs 10
-python train.py --model ResNet50 --dataset CIFAR100 --epochs 15
